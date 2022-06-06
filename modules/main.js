6ee1bf410c4482bbb1c56eac59deeaca7dbc7712
Zelalem1222 <zelalemfissha394@gmail.com>
2022-06-06 17:36:51 +0300

    modify the page

--- modules/main.js
+++ modules/main.js
@@ -1,5 +1,7 @@
-import Books from './constructor.js'
-import { link1, link2 , link3 } from './pages.js';
+import Books from './constructor.js';
+
+/* eslint-disable */
+import { link1, link2, link3 } from './pages.js';
 import { DateTime } from './luxon.js';
 
 const booksContainer = document.querySelector('.books-container');
@@ -8,13 +10,14 @@ const form = document.querySelector('form');
 const book = new Books();
 book.showBooks();
 
+  
 // Add book and update UI as well as localStorage
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   book.updateBooks();
   form.reset();
-  console.log(book)
+  console.log(book);
 });
 
 // Remove that Book
@@ -22,7 +25,6 @@ form.addEventListener('submit', (e) => {
 booksContainer.addEventListener('click', (e) => {
   book.removeBook(e);
 });
-
 
 const time = () => {
   const timeDisplay = document.getElementById('time');
@@ -36,4 +38,3 @@ const time = () => {
   });
 };
 setInterval(time, 1000);
-
